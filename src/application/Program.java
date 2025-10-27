edbc74d82d26b0f44f05ce8ebb375cacc63cdd89
MatheusLeiteCarneiro <matheus.leite.carneiro@gmail.com>
2025-10-27 10:52:03 -0300

    creating the product ArrayList, Scanner and setting Locale

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,21 @@
 package application;
 
+
+
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
 	}
 
